970a928dfb9d5f0f19e4fe12da437f002553706f
Anton Dyakov <dyakant@gmail.com>
2023-07-21 23:50:18 +0300

    project init

---
+++ src/main/java/ru/gitant/BinanceTraderTestApp.java
@@ -0,0 +1,19 @@
+package ru.gitant;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Hello world!
+ *
+ */
+@Slf4j
+public class BinanceTraderTestApp
+{
+    public static void main( String[] args )
+    {
+        log.info("Start");
+        log.debug("Start");
+        log.error("Start");
+        System.out.println( "Hello World!" );
+    }
+}
